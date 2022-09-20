90a8c98eeff91d029bae48a372abb14967ff7217
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-09-20 18:58:36 +0200

    add template id

--- functions/src/functions/sendResetPasswordEmail.ts
+++ functions/src/functions/sendResetPasswordEmail.ts
@@ -1,20 +1,19 @@
 import * as functions from "firebase-functions";
-import sgMail = require("@sendgrid/mail");
+import { setApiKey, send, MailDataRequired } from "@sendgrid/mail";
 import { PasswordResetInfo } from "../entities/PasswordResetInfo";
 
 export const sendEmail = functions
   .region("europe-west2")
   .https.onCall(async (passwordResetInfo: PasswordResetInfo) => {
-    sgMail.setApiKey(
-      "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");
-    const msg = {
+    setApiKey("");
+    const msg: MailDataRequired = {
       to: passwordResetInfo.email,
       from: "denardincarlodev@gmail.com",
       subject: "Recupero password MountainApp",
       text: `Il codice per recuperare la password è ${passwordResetInfo.code}`,
+      templateId: "d-4e9220fbe02d402ea13c5c236c2095a4",
     };
-    sgMail
-      .send(msg)
+    send(msg)
       .then(() => {
         console.log("Email sent");
       })
