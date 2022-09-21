add30414e00ea331c6f39f6aaed339e0b2418f97
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-09-21 09:52:24 +0200

    change send grid import to solve the setApiKey error

--- functions/src/functions/sendResetPasswordEmail.ts
+++ functions/src/functions/sendResetPasswordEmail.ts
@@ -1,23 +1,23 @@
 import * as functions from "firebase-functions";
-import { setApiKey, send, MailDataRequired } from "@sendgrid/mail";
 import { PasswordResetInfo } from "../entities/PasswordResetInfo";
+import * as sendGrid from "@sendgrid/mail";
 
 export const sendEmail = functions
   .region("europe-west2")
   .https.onCall(async (passwordResetInfo: PasswordResetInfo) => {
-    setApiKey("");
-    const msg: MailDataRequired = {
+    sendGrid.setApiKey("");
+
+    const msg: sendGrid.MailDataRequired = {
       to: passwordResetInfo.email,
       from: "denardincarlodev@gmail.com",
       subject: "Recupero password MountainApp",
       text: `Il codice per recuperare la password è ${passwordResetInfo.code}`,
-      templateId: "d-4e9220fbe02d402ea13c5c236c2095a4",
     };
-    send(msg)
-      .then(() => {
-        console.log("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+
+    try {
+      sendGrid.send(msg);
+      console.log("Email sent");
+    } catch (error) {
+      console.error(error);
+    }
   });
