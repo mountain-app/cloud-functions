8cb217cdf7375d807664efd293637f8292ed979b
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-09-23 15:54:33 +0200

    removed parameters from sendResetPassword function for testint

--- functions/src/functions/sendResetPasswordEmail.ts
+++ functions/src/functions/sendResetPasswordEmail.ts
@@ -1,22 +1,17 @@
 import * as functions from "firebase-functions";
 import * as sendGrid from "@sendgrid/mail";
-import * as dotenv from "dotenv";
 
 export const sendEmail = functions
   .region("europe-west2")
-  .https.onCall(async ({ email, code }: { email: string; code: string }) => {
-    dotenv.config();
-
-    console.log(process.env.SENDGRID_API_KEY);
-
+  .https.onCall(async () => {
     sendGrid.setApiKey(process.env.SENDGRID_API_KEY || "");
 
     try {
       const msg: sendGrid.MailDataRequired = {
-        to: email,
+        to: "denardincarlo@gmail.com",
         from: "denardincarlodev@gmail.com",
         subject: "Recupero password MountainApp",
-        text: `Il codice per recuperare la password è ${code}`,
+        text: "Il codice per recuperare la password è 1234",
         templateId: "d-3233deca4c654dfe96691847f3aea421",
         dynamicTemplateData: {
           name: "Carlodev",
