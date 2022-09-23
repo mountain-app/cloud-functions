fea43b1cd1e99f40ce723b7e4a05b755c265bf6c
carlodenardin <64202858+carlodenardin@users.noreply.github.com>
2022-09-23 15:01:09 +0200

    sendgrid key

--- functions/src/functions/sendResetPasswordEmail.ts
+++ functions/src/functions/sendResetPasswordEmail.ts
@@ -1,9 +1,14 @@
 import * as functions from "firebase-functions";
 import * as sendGrid from "@sendgrid/mail";
+import * as dotenv from "dotenv";
 
 export const sendEmail = functions
   .region("europe-west2")
   .https.onCall(async ({ email, code }: { email: string; code: string }) => {
+    dotenv.config();
+
+    console.log(process.env.SENDGRID_API_KEY);
+
     sendGrid.setApiKey(process.env.SENDGRID_API_KEY || "");
 
     try {
